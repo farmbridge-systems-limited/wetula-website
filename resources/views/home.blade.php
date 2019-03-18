d17caf7b71da694d6d2923fd99ee136a08e0867a
Bernard D. White <danquahwhite@gmail.com>
2019-03-18 14:34:38 +0000

    Designed Footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -381,6 +381,241 @@
                 </div>
             </div>
 
+            <footer>
+                <div class="container pt-10 pb-10">
+                    <div id="logo-plus-settings-container">
+                        <div class="logo">
+                            <img src="{{asset('images/wetula_logo_dark.svg')}}" alt="">
+                        </div>
+                        <div class="footer-geo-settings mt-8">
+                            <div class="language-setting">
+                                <div class="settings-container fg-gray">
+                                    <div class="pos-relative">
+                                        <a class="button link dropdown-toggle"><span
+                                                class="mif-language mif-2x mr-4 fg-white"></span> English(UK)</a>
+                                        <ul class="d-menu" data-role="dropdown">
+                                            <li><a href="#">English (UK)</a></li>
+                                            <li><a href="#">French</a></li>
+                                            <li><a href="#">Espanol</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="pos-relative">
+                                        <a class="button link dropdown-toggle"><span
+                                                class="mif-earth mif-2x mr-4 fg-white"></span> Ghana</a>
+                                        <ul class="d-menu" data-role="dropdown">
+                                            <li><a href="#">Africa</a></li>
+                                            <li><a href="#">Asia</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div id="site-links">
+                        <div class="linkset" id="major-links">
+                            <ul style=" list-style: none inside;">
+                                <li class="no-border-bottom">
+                                    <a class="text-upper fg-orange-hover no-decor fg-white" href="#">
+                                        <span class="mif-organization mif-2x mr-4 fg-white"></span>
+                                        <span class="mt-2-minus text-bold">About Us</span>
+                                    </a>
+                                </li>
+                                <li class="no-border-bottom">
+                                    <a class="text-upper fg-orange-hover no-decor fg-white" href="#">
+                                        <span class="mif-user mif-2x mr-4 fg-white"></span>
+                                        <span class="mt-2-minus text-bold">Client Login</span>
+                                    </a>
+                                </li>
+                                <li class="no-border-bottom">
+                                    <a class="text-upper fg-orange-hover no-decor fg-white" href="#">
+                                        <span class="mif-user-plus mif-2x mr-4 fg-white"></span>
+                                        <span class="mt-2-minus text-bold">Join our Team</span>
+                                    </a>
+                                </li>
+                                <li class="no-border-bottom">
+                                    <a class="text-upper fg-orange-hover no-decor fg-white" href="#">
+                                        <span class="mif-users mif-2x mr-4 fg-white"></span>
+                                        <span class="mt-2-minus text-bold">Our Partners</span>
+                                    </a>
+                                </li>
+                                <li class="no-border-bottom">
+                                    <a class="text-upper fg-orange-hover no-decor fg-white" href="#">
+                                        <span class="mif-files-empty mif-2x mr-4 fg-white"></span>
+                                        <span class="mt-2-minus text-bold">Legal & Policies</span>
+                                    </a>
+                                </li>
+
+                            </ul>
+                        </div>
+                        <div class="linkset" id="link-set-1" style="margin-top: 35px">
+                            <ul style=" list-style: none inside;">
+                                <li><p class="text-bold text-upper fg-white">Operation Sectors</p></li>
+                                <li class="no-border-bottom">
+                                    <a class="no-decor fg-orange-hover fg-gray" href="#">
+                                        <span class="mt-2-minus">Agriculture</span>
+                                    </a>
+                                </li>
+                                <li><p class="text-bold text-upper mt-4 fg-white">Products</p></li>
+                                <li class="no-border-bottom">
+                                    <a class="no-decor fg-orange-hover fg-gray" href="#">
+                                        <span class="mt-2-minus">Wetula Faire Scale</span>
+                                    </a>
+                                </li>
+
+
+                                <li><p class="text-bold text-upper mt-4 fg-white">Services</p></li>
+                                <li class="no-border-bottom">
+                                    <a class="no-decor fg-orange-hover fg-gray" href="#">
+                                        <span class="mt-2-minus">Wetula APIS</span>
+                                    </a>
+                                </li>
+                                <li class="no-border-bottom">
+                                    <a class="no-decor fg-orange-hover fg-gray" href="#">
+                                        <span class="mt-2-minus">Business Development</span>
+                                    </a>
+                                </li>
+
+
+                            </ul>
+                        </div>
+                        <div class="linkset" style="margin-top: 20px" id="link-set-2">
+                            <ul style=" list-style: none inside;">
+                                <li><p class="text-bold text-upper mt-4 fg-white">Client Categories</p></li>
+                                <li class="no-border-bottom">
+                                    <a class="no-decor fg-orange-hover fg-gray" href="#">
+                                        <span class="mt-2-minus">Farmers</span>
+                                    </a>
+                                </li>
+                                <li class="no-border-bottom">
+                                    <a class="no-decor fg-orange-hover fg-gray" href="#">
+                                        <span class="mt-2-minus">Product Retailers</span>
+                                    </a>
+                                </li>
+                                <li class="no-border-bottom">
+                                    <a class="no-decor fg-orange-hover fg-gray" href="#">
+                                        <span class="mt-2-minus">License Buying Companies</span>
+                                    </a>
+                                </li>
+
+                                <li class="no-border-bottom">
+                                    <a class="no-decor fg-orange-hover fg-gray" href="#">
+                                        <span class="mt-2-minus">Input Suppliers</span>
+                                    </a>
+                                </li>
+
+                                <li class="no-border-bottom">
+                                    <a class="no-decor fg-orange-hover fg-gray" href="#">
+                                        <span class="mt-2-minus">Dealer Services</span>
+                                    </a>
+                                </li>
+
+                                <li class="no-border-bottom">
+                                    <a class="no-decor fg-orange-hover fg-gray" href="#">
+                                        <span class="mt-2-minus">Certification Agencies</span>
+                                    </a>
+                                </li>
+
+                                <li class="no-border-bottom">
+                                    <a class="no-decor fg-orange-hover fg-gray" href="#">
+                                        <span class="mt-2-minus">Crop Marketers</span>
+                                    </a>
+                                </li>
+
+
+                                <li class="no-border-bottom">
+                                    <a class="no-decor fg-orange-hover fg-gray" href="#">
+                                        <span class="mt-2-minus">Co-operatives/ Aggregators</span>
+                                    </a>
+                                </li>
+
+                                <li class="no-border-bottom">
+                                    <a class="no-decor fg-orange-hover fg-gray" href="#">
+                                        <span class="mt-2-minus">NGOs</span>
+                                    </a>
+                                </li>
+
+
+                                <li class="no-border-bottom">
+                                    <a class="no-decor fg-orange-hover fg-gray" href="#">
+                                        <span class="mt-2-minus">Financial Institutions</span>
+                                    </a>
+                                </li>
+
+                                <li class="no-border-bottom">
+                                    <a class="no-decor fg-orange-hover fg-gray" href="#">
+                                        <span class="mt-2-minus">Government</span>
+                                    </a>
+                                </li>
+
+
+                            </ul>
+                        </div>
+                        <div class="linkset" style="margin-top: 35px" id="link-set-3">
+                            <ul style=" list-style: none inside;">
+                                <li><p class="text-bold text-upper fg-white">Location Details</p></li>
+                                <li class="no-border-bottom">
+                                    <p class="no-decor fg-gray" href="#">
+                                        <span class="mt-2-minus">Room 26, Bishop Arthur Center</span><br>
+                                        <span>St. Cyprians Anglican Cathedral</span> <br>
+                                        <span>Maxwell Road, Fante - New Town</span> <br>
+                                        <span>Kumasi - Ghana</span> <br><br>
+                                        <button class="button rounded bg-white fg-darkGreen"><span
+                                                class="mif-map2 mif-2x"></span> View on Map
+                                        </button>
+                                    </p>
+                                </li>
+
+                                <li><p class="text-bold mt-8 text-upper fg-white">Contact Details</p></li>
+                                <li class="no-border-bottom">
+                                    <p class="no-decor fg-gray" href="#">
+                                        <span class="mif-phone fg-white "></span> <span> 233 244 067 500</span><br>
+                                        <span class="mif-mail fg-white "></span> <span> support@wetula.com</span><br>
+                                    </p>
+                                </li>
+
+
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class=" pt-2 pb-0 bg-white">
+                    <div class="container socialmedia d-flex flex-justify-center">
+                        <ul class="inline-list">
+                            <li>
+                                <a class="fg-green fg-orange-hover" href="#"><span
+                                        class="mif-facebook mif-4x"></span></a>
+                            </li>
+                            <li>
+                                <a class="fg-green fg-orange-hover" href="#"><span
+                                        class="mif-twitter mif-4x"></span></a>
+                            </li>
+                            <li>
+                                <a class="fg-green fg-orange-hover" href="#"><span class="mif-instagram mif-4x"></span></a>
+                            </li>
+                            <li>
+                                <a class="fg-green fg-orange-hover" href="#"><span
+                                        class="mif-linkedin mif-4x"></span></a>
+                            </li>
+                            <li>
+                                <a class="fg-green fg-orange-hover" href="#"><span class="mif-github mif-4x"></span></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="pt-2 pb-2 bg-darkGreen">
+                    <div class="container">
+                        <p class="text-center small fg-gray">
+                            <a class="no-decor fg-orange-hover fg-white" href="#">Terms of Service</a> |
+                            <a class="no-decor fg-orange-hover fg-white" href="#"> Privacy Policy</a> |
+                            <a class="no-decor fg-orange-hover fg-white" href="#">Disclaimer & Waiver of Liability</a> |
+                            <a class="no-decor fg-orange-hover fg-white" href="#">Data Protection Policy</a> |
+                            <a class="no-decor fg-orange-hover fg-white" href="#">Corporate Assurance</a>
+                        </p>
+                    </div>
+                </div>
+            </footer>
+
         </div>
 
 
